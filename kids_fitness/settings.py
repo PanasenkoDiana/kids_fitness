2ca0f946d61a39bff54fbb6bf4366331e2ec6ea7
Diana <shad0wdrag0n19902@gmail.com>
2024-08-31 16:50:23 +0300

    choose templates

--- kids_fitness/settings.py
+++ kids_fitness/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-kwehe@-%^t1n#4u8(yyup+c+%b*x_hmfqvfg@iyk2so49zi$na
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['kidsfitness.pythonanywhere.com']
+ALLOWED_HOSTS = ['kidsfitness.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
